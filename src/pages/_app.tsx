fad507c6ff1a77c100a92d14db8ce8aaeb813218
Guilherme Valeriano <guilherme.hvaleriano@gmail.com>
2023-03-29 18:07:42 -0300

    Challenge conclusion

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,8 +1,28 @@
-import { AppProps } from 'next/app';
-import '../styles/globals.scss';
+import { AppProps } from "next/app";
+import { ChakraProvider } from "@chakra-ui/react";
+import { theme } from "../styles/theme";
+import NextNProgress from "nextjs-progressbar";
 
-function MyApp({ Component, pageProps }: AppProps): JSX.Element {
-  return <Component {...pageProps} />;
+// Import Swiper styles
+import "swiper/scss";
+import "swiper/scss/navigation";
+import "swiper/scss/pagination";
+import "swiper/scss/scrollbar";
+
+import "../styles/slider.scss";
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <NextNProgress
+        color="#FFBA08"
+        startPosition={0.3}
+        stopDelayMs={200}
+        height={5}
+      />
+      <Component {...pageProps} />
+    </ChakraProvider>
+  );
 }
 
-export default MyApp;
+export default App;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,36 +1,76 @@
-import { GetStaticProps } from 'next';
-
-import { getPrismicClient } from '../services/prismic';
-
-import commonStyles from '../styles/common.module.scss';
-import styles from './home.module.scss';
-
-interface Post {
-  uid?: string;
-  first_publication_date: string | null;
-  data: {
-    title: string;
-    subtitle: string;
-    author: string;
-  };
-}
-
-interface PostPagination {
-  next_page: string;
-  results: Post[];
-}
+import { Flex, Heading } from "@chakra-ui/react";
+import { GetStaticProps } from "next";
+import Prismic from "@prismicio/client";
+import Head from "next/head";
+import { Banner } from "../components/Banner";
+import { Caracteristicas } from "../components/Caracteristicas";
+import { Header } from "../components/Header";
+import { Separador } from "../components/Separador";
+import { Slider } from "../components/Slider";
+import { getPrismicClient } from "../services/prismic";
 
 interface HomeProps {
-  postsPagination: PostPagination;
+  continents: {
+    slug: string;
+    title: string;
+    summary: string;
+    image: string;
+  }[];
 }
 
-// export default function Home() {
-//   // TODO
-// }
+export default function Home({ continents }: HomeProps) {
+  return (
+    <Flex direction="column">
+      <Head>
+        <title>WorldTrip - Home</title>
+        <meta property="og:image" content="/ogimage.png" />
+        <meta property="og:image:secure_url" content="/ogimage.png" />
+        <meta name="twitter:image" content="/ogimage.png" />
+        <meta name="twitter:image:src" content="/ogimage.png" />
+        <meta property="og:title" content="WorldTrip" />
+        <meta name="twitter:title" content="WorldTrip" />
+      </Head>
 
-// export const getStaticProps = async () => {
-//   // const prismic = getPrismicClient({});
-//   // const postsResponse = await prismic.getByType(TODO);
+      <Header />
+      <Banner />
+      <Caracteristicas />
+      <Separador />
 
-//   // TODO
-// };
+      <Heading
+        textAlign="center"
+        fontWeight="500"
+        mb={["5", "14"]}
+        fontSize={["lg", "3xl", "4xl"]}
+      >
+        Vamos nessa?
+        <br />
+        Então escolha seu continente
+      </Heading>
+
+      <Slider continents={continents} />
+    </Flex>
+  );
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const prismic = getPrismicClient();
+
+  const response = await prismic.query([
+    Prismic.Predicates.at("document.type", "c"),
+  ]);
+
+  const continents = response.results.map((continent) => {
+    return {
+      slug: continent.uid,
+      title: continent.data.title,
+      summary: continent.data.summary,
+      image: continent.data.slider_image.url,
+    };
+  });
+
+  return {
+    props: {
+      continents,
+    },
+  };
+};
